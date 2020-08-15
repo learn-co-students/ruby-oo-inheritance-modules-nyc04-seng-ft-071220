d9e60ad6e5e634194befe18d72abcb1227c64c5c
emjose <emjose@gmail.com>
2020-08-15 12:53:56 -0400

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    
+    # This is a module in dance_module.rb. include keyword allows us to use all of the methods of 
+    # the included module as instance methods (extend keyword allows module to be used as class methods)
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
